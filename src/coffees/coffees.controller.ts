7eae3f44be28f05eddba42a1ceeea456f791b4c0
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-26 15:20:08 +1030

    add coffees show and create route and controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('/')
   index() {
     return 'all coffees';
+  }
+
+  @Get('/:id')
+  show(@Param() params) {
+    return `The Coffee #${params.id}`;
+  }
+
+  @Post('/')
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() body) {
+    return `${JSON.stringify(body)} has been created as a coffee.`;
   }
 }
