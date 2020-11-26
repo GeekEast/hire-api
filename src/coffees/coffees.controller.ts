7569217d43c520d728f38bb4053fa1d332a2d6e1
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-26 15:26:40 +1030

    add coffees delete, patch and put route and controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
+  Patch,
   Post,
+  Put,
 } from '@nestjs/common';
 
 @Controller('coffees')
@@ -21,8 +22,22 @@ export class CoffeesController {
   }
 
   @Post('/')
-  @HttpCode(HttpStatus.CREATED)
   create(@Body() body) {
     return `${JSON.stringify(body)} has been created as a coffee.`;
+  }
+
+  @Delete('/:id')
+  remove(@Param() params) {
+    return `The Coffee #${params.id} is removed`;
+  }
+
+  @Patch('/:id')
+  partial_update(@Param() params, @Body() body) {
+    return `The Coffee #${params.id} is updated`;
+  }
+
+  @Put('/:id')
+  update(@Param() params, @Body() body) {
+    return `The Coffee #${params.id} is updated`;
   }
 }
