0bfef75f6a9de296158f0be285be9428501f6a69
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-26 15:01:54 +1030

    docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // register with the root module
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
