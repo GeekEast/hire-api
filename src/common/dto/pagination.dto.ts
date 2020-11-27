f3721844844c4fdb208293b37bf6b85acd451486
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-28 08:58:39 +1030

    you no longer need validate pagination types when you  enableImplicitConversion in main.ts

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,10 @@
-import { IsNumber, IsOptional, IsPositive } from 'class-validator';
+import { IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationDto {
-  @IsNumber()
   @IsOptional()
   @IsPositive()
   take?: number;
 
-  @IsNumber()
   @IsOptional()
   @IsPositive()
   offset?: number;
