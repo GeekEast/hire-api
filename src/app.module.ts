3de0df81617a04ae54f52b110e657b9e3debdee7
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-28 15:05:51 +1030

    install password and generate auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from 'app.service';
 import { CompaniesModule } from 'companies/companies.module';
 import { UsersModule } from 'users/users.module';
 import { VacanciesModule } from 'vacancies/vacancies.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { VacanciesModule } from 'vacancies/vacancies.module';
     CompaniesModule,
     UsersModule,
     VacanciesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
