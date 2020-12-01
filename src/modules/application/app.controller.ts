7f1e9493759ed894769ec919e0a20b436d13da41
ZenGeek <xiang.tan.p2109@gmail.com>
2020-12-01 14:22:29 +1030

    protect root route

--- src/modules/application/app.controller.ts
+++ src/modules/application/app.controller.ts
@@ -1,13 +1,11 @@
 import { AppService } from './app.service';
 import { Controller, Get } from '@nestjs/common';
-import { SkipJwt } from 'modules/auth/decorators/skipJwt.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @SkipJwt()
   getHello(): string {
     return 'Welcome to the Hire API.';
   }
