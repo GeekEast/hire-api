43aef63926edc58df7c35280f49b65b0ada4b869
ZenGeek <xiang.tan.p2109@gmail.com>
2020-12-01 14:18:32 +1030

    add permission for vacancy

--- src/modules/vacancies/vacancies.controller.ts
+++ src/modules/vacancies/vacancies.controller.ts
@@ -1,3 +1,4 @@
+import { RolesGuard } from 'modules/auth/guards/roles.guard';
 import { UpdateVacancyDto } from './dto/update.dto';
 import { VacanciesService } from 'modules/vacancies/vacancies.service';
 import {
@@ -10,9 +11,12 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ListVacancyPaginationDto } from './dto/list.dto';
 import { CreateVacancyDto } from './dto/create.dto';
+import { Role } from 'modules/auth/decorators/roles.decorator';
+import { RoleEnum } from 'modules/auth/enums/role.enum';
 
 @Controller('vacancies')
 export class VacanciesController {
@@ -29,16 +33,22 @@ export class VacanciesController {
   }
 
   @Post('')
+  @Role(RoleEnum.Admin)
+  @UseGuards(RolesGuard)
   create(@Body() createVacancyDto: CreateVacancyDto) {
     return this.vacanciesService.create(createVacancyDto);
   }
 
   @Put('/:id')
+  @Role(RoleEnum.Admin)
+  @UseGuards(RolesGuard)
   update(@Param('id') id: string, @Body() updateVacancyDto: UpdateVacancyDto) {
     return this.vacanciesService.update(id, updateVacancyDto);
   }
 
   @Patch('/:id')
+  @Role(RoleEnum.Admin)
+  @UseGuards(RolesGuard)
   partial_update(
     @Param('id') id: string,
     @Body() updateVacancyDto: UpdateVacancyDto,
@@ -47,6 +57,8 @@ export class VacanciesController {
   }
 
   @Delete('/:id')
+  @Role(RoleEnum.Admin)
+  @UseGuards(RolesGuard)
   remove(@Param() id: string) {
     return this.vacanciesService.remove(id);
   }
